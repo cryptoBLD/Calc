ddde686277e001ac5b4adb3ff0f0081d3e700dc8
cryptoBLD <yann@gaspoz-fleiner.com>
2021-09-09 22:21:29 +0200

    finished v1

--- src/com/javapointers/javase/Main.java
+++ src/com/javapointers/javase/Main.java
@@ -38,6 +38,7 @@ public class Main implements ActionListener{
         c.gridx = 1;
         c.gridy =0;
         c.gridwidth = 1;
+        label.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(label, c);
 
         JButton plus = new JButton("+");
@@ -45,6 +46,7 @@ public class Main implements ActionListener{
         c.gridy = 1;
         plus.setActionCommand("+");
         plus.addActionListener(this);
+        plus.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(plus,c);
 
         JButton minus = new JButton("-");
@@ -52,6 +54,7 @@ public class Main implements ActionListener{
         c.gridy = 1;
         minus.setActionCommand("-");
         minus.addActionListener(this);
+        minus.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(minus,c);
 
         JButton times = new JButton("*");
@@ -59,6 +62,7 @@ public class Main implements ActionListener{
         c.gridy = 1;
         times.setActionCommand("*");
         times.addActionListener(this);
+        times.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(times,c);
 
         JButton seven = new JButton("7");
@@ -66,6 +70,7 @@ public class Main implements ActionListener{
         c.gridy = 2;
         seven.setActionCommand("7");
         seven.addActionListener(this);
+        seven.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(seven,c);
 
         JButton eight = new JButton("8");
@@ -73,6 +78,7 @@ public class Main implements ActionListener{
         c.gridy = 2;
         eight.setActionCommand("8");
         eight.addActionListener(this);
+        eight.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(eight,c);
 
         JButton nine = new JButton("9");
@@ -80,6 +86,7 @@ public class Main implements ActionListener{
         c.gridy = 2;
         nine.setActionCommand("9");
         nine.addActionListener(this);
+        nine.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(nine,c);
 
         JButton four = new JButton("4");
@@ -87,6 +94,7 @@ public class Main implements ActionListener{
         c.gridy = 3;
         four.setActionCommand("4");
         four.addActionListener(this);
+        four.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(four,c);
 
         JButton five = new JButton("5");
@@ -94,6 +102,7 @@ public class Main implements ActionListener{
         c.gridy = 3;
         five.setActionCommand("5");
         five.addActionListener(this);
+        five.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(five,c);
 
         JButton six = new JButton("6");
@@ -101,6 +110,7 @@ public class Main implements ActionListener{
         c.gridy = 3;
         six.setActionCommand("6");
         six.addActionListener(this);
+        six.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(six,c);
 
         JButton one = new JButton("1");
@@ -108,6 +118,7 @@ public class Main implements ActionListener{
         c.gridy = 4;
         one.setActionCommand("1");
         one.addActionListener(this);
+        one.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(one,c);
 
         JButton two = new JButton("2");
@@ -115,6 +126,7 @@ public class Main implements ActionListener{
         c.gridy = 4;
         two.setActionCommand("2");
         two.addActionListener(this);
+        two.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(two,c);
 
         JButton three = new JButton("3");
@@ -122,6 +134,7 @@ public class Main implements ActionListener{
         c.gridy = 4;
         three.setActionCommand("3");
         three.addActionListener(this);
+        three.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(three,c);
 
         JButton divide = new JButton(":");
@@ -129,6 +142,7 @@ public class Main implements ActionListener{
         c.gridy = 5;
         divide.setActionCommand("/");
         divide.addActionListener(this);
+        divide.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(divide,c);
 
         JButton zero = new JButton("0");
@@ -136,6 +150,7 @@ public class Main implements ActionListener{
         c.gridy = 5;
         zero.setActionCommand("0");
         zero.addActionListener(this);
+        zero.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(zero,c);
 
         JButton point = new JButton(".");
@@ -143,6 +158,7 @@ public class Main implements ActionListener{
         c.gridy = 5;
         point.setActionCommand(".");
         point.addActionListener(this);
+        point.setFont(new Font("Arial", Font.PLAIN, 40));
         panel.add(point,c);
 
         frame.add(panel, BorderLayout.CENTER);
